0df410a724f083641d290a756a111d696a237bf6
Bruna Verrone <ti.agosem@hotmail.com>
2025-01-31 21:14:34 -0300

    Add project section styles and enhance form validation

--- js/script.js
+++ js/script.js
@@ -1,36 +1,58 @@
 let nome = window.document.getElementById('nome');
 let email = document.querySelector('#email');
-let nomeOk=false;
-let emailOk=false;
+let nomeOk = false;
+let emailOk = false;
+let assuntoOk = false;
 
-function validaNome(){
-    let txtNome=document.querySelector('#txtNome')
-    if(nome.value.length < 3 ){
-        txtNome.innerHTML='Nome inválido!'
-        txtNome.style.color='red'
-    }else{
-        txtNome.innerHTML='Nome válido!'
-        txtNome.style.color='green'
-        nomeOk=true
+function currentYear() {
+    let ano = new Date().getFullYear();
+    return ano;
+}
+
+function validName() {
+    let txtNome = document.querySelector('#txtNome');
+    if (nome.value.length < 3) {
+        txtNome.innerHTML = 'Nome inválido!';
+        txtNome.style.color = 'red';
+    } else {
+        txtNome.innerHTML = 'Nome válido!';
+        txtNome.style.color = 'green';
+        nomeOk = true;
     }
 }
 
-function validaEmail(){
-    let txtEmail = document.querySelector('#txtEmail')
-    if(email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1){
-        txtEmail.innerHTML='E-mail inválido!'
-        txtEmail.style.color='red'
-    }else{
-        txtEmail.innerHTML='E-mail válido!'
-        txtEmail.style.color='green'
-        emailOk=true
+function validEmail() {
+    let txtEmail = document.querySelector('#txtEmail');
+    if (email.value.indexOf('@') === -1 || email.value.indexOf('.') === -1) {
+        txtEmail.innerHTML = 'E-mail inválido!';
+        txtEmail.style.color = 'red';
+    } else {
+        txtEmail.innerHTML = 'E-mail válido!';
+        txtEmail.style.color = 'green';
+        emailOk = true;
     }
 }
 
-function enviar(){
-    if(nomeOk==true && emailOk ==true){
-        alert('O formulário foi enviado com sucesso! 😄')
-    }else{
-        alert('Preencha o formulário corretamente antes de enviar!🤔')
+function validAssunto() {
+    let txtAssunto = document.querySelector('#txtAssunto');
+    if (document.querySelector('#assunto').value.length < 10) {
+        txtAssunto.innerHTML = 'Assunto muito curto!';
+        txtAssunto.style.color = 'red';
+    } else {
+        txtAssunto.innerHTML = 'Assunto válido!';
+        txtAssunto.style.color = 'green';
+        assuntoOk = true;
     }
 }
+
+function send(event) {
+    event.preventDefault();
+    if (nomeOk && emailOk && assuntoOk) {
+        alert('O formulário foi enviado com sucesso! 😄');
+    } else {
+        alert('Preencha o formulário corretamente antes de enviar! 🤔');
+    }
+}
+
+document.getElementById("contact-form").addEventListener("submit", send);
+
